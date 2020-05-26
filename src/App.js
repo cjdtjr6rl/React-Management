cf96894a67d5bf11d6aec88fef89eafe3780455d
cjdtjr6rl <gud0415@gmail.com>
2020-05-26 21:51:43 +0900

    feat : material ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
 import Customer from './component/Customer' // Customer.js 파일을 import하여 출력할 수 있도록 구현
 import './App.css';
+/* material-ui를 이용하여 table 생성 */
+import Paper from '@material-ui/core/Paper'
+import Table from '@material-ui/core/Table'
+import TableHead from '@material-ui/core/TableHead'
+import TableBody from '@material-ui/core/TableBody'
+import TableRow from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
+import { withStyles } from '@material-ui/core/styles'
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080 // 가로 길이가 1080px보다 작아지면 가로 스크롤바가 생기게 함
+  }
+})
 
 const customers = [
   {
@@ -31,26 +50,41 @@ const customers = [
 
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        {
-          customers.map(c => { // map함수를 사용함으로써 소스 코드가 훨씬 간결
-            return (
-            <Customer
-              key = {c.id} // map을 사용할 시 각각 구분을 할 수 있는 key값을 넣어주어야 함
-              id = {c.id}
-              image = {c.image}
-              name = {c.name}
-              birthday = {c.birthday}
-              gender = {c.gender}
-              job = {c.job}
-            />
-            );
-          })
-        }
-      </div>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {
+              customers.map(c => { // map함수를 사용함으로써 소스 코드가 훨씬 간결
+                return (
+                <Customer
+                  key = {c.id} // map을 사용할 시 각각 구분을 할 수 있는 key값을 넣어주어야 함
+                  id = {c.id}
+                  image = {c.image}
+                  name = {c.name}
+                  birthday = {c.birthday}
+                  gender = {c.gender}
+                  job = {c.job}
+                />
+                );
+              })
+            }
+          </TableBody>
+        </Table>
+      </Paper>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
