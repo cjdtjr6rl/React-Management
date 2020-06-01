d010bf8040673eda831ade23f3863b49d77de360
cjdtjr6rl <gud0415@gmail.com>
2020-06-01 15:49:23 +0900

    feat : create customer db & node.js db connection setting

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,34 +7,28 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+connection.connect();
+
+// database에서 가져온 정보를 넘겨주도록 함
 app.get('/api/customers', (req, res) => { // restApi
     // client가 요청을 하게 되면 아래와 같은 파일을 json 형식으로 불러오게 되서 client에게 보여줌
-    res.send([
-        {
-          'id': 1,
-          'image': 'https://placeimg.com/64/64/any',
-          'name': '이준형',
-          'birthday': '940415',
-          'gender': '남자',
-          'job': '대학생'
-        },
-        {
-          'id': 2,
-          'image': 'https://placeimg.com/64/65/any',
-          'name': '용범중',
-          'birthday': '961234',
-          'gender': '남자',
-          'job': '프로그래머'
-        },
-        {
-          'id': 3,
-          'image': 'https://placeimg.com/64/66/any',
-          'name': '심지희',
-          'birthday': '950508',
-          'gender': '여자',
-          'job': '직장인'
-        }
-      ]);
+    connection.query(
+      "SELECT * FROM CUSTOMER",
+      (err, rows, fields) => {
+        res.send(rows); // 사용자들에게 rows의 내용을 보여주는 것
+      }
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
