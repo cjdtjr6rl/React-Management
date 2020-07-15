d69a4037fd22930c706d5563c0e06ac42697c2bd
cjdtjr6rl <gud0415@gmail.com>
2020-07-15 17:25:09 +0900

    feat : edit classAdd

--- client/src/component/ClassAdd.js
+++ client/src/component/ClassAdd.js
@@ -105,17 +105,12 @@ class ClassAdd extends React.Component {
                 <Dialog open={this.state.open} onClose={this.handleClose}>
                     <DialogTitle>학과 추가</DialogTitle>
                     <DialogContent>
-                        <input className={classes.hidden} accept="image/*" id="raised-button-file" type="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}/><br/>
-                        <label htmlFor="raised-button-file">
-                            <Button variant="contained" color="primary" component="span" name="file">
-                                {this.state.fileName === "" ? "프로필 이미지 선택" : this.state.fileName}
-                            </Button>
-                        </label>
+                        <input className={classes.hidden} accept="image/*" id="raised-button-file" type="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}/>
                         <br/>
-                        <TextField label="이름" type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange}/><br/>
-                        <TextField label="생년월일" type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange}/><br/>
-                        <TextField label="성별" type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange}/><br/>
-                        <TextField label="직업" type="text" name="job" value={this.state.job} onChange={this.handleValueChange}/><br/>
+                        <TextField label="학과" type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange}/><br/>
+                        <TextField label="학부" type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange}/><br/>
+                        <TextField label="과목이름" type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange}/><br/>
+                        <TextField label="학점" type="text" name="job" value={this.state.job} onChange={this.handleValueChange}/><br/>
                     </DialogContent>
                     <DialogActions>
                         <Button variant="contained" color="primary" onClick={this.handleFormSubmit}>추가</Button>
